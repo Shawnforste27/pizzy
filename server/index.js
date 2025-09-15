d9b0ede01b3c722ccf9c7f7fc337890c4e8ea1d8
Ambikesh Kushwaha <147277970+Shawnforste27@users.noreply.github.com>
2025-09-15 21:47:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
      cors: {
-    origin: "http://localhost:5173", // production में specific domain डालना
+    origin: "http://localhost:5173", 
     methods: ["GET", "POST"],
     credentials: true  
   }
